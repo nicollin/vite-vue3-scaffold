e1895e40e7e14559fa8ee7a4a532f1ee2078079c
nicollin <17759076752@163.com>
2022-03-08 23:51:53 +0800

    集成VueRouter、Vuex、Element Plus、Axios

---
+++ src/views/axios.vue
@@ -0,0 +1,20 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "../utils/axios";
+
+export default defineComponent({
+    setup() {
+        axios.get('/user/nicollin').then((res) => {
+            console.log('res:', res);
+        }).catch(err => {
+            console.log('err:', err);
+        })
+    }
+})
+
+</script>
+
+<style scoped>
+</style>
